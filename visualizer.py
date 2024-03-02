d87b3cfee6f1fb7fc7052e922ed96bae33ba8022
Beat Hagenlocher <bah@posteo.de>
2024-03-03 00:13:28 +0100

    Get the video to run

--- visualizer.py
+++ visualizer.py
@@ -47,18 +47,21 @@ def make_vid(data_array):
     t, x, y = data_array.shape
     for i in range(t):
         rgb_array = array_to_rgb(data_array[i])
-        cv2.imwrite('out/' +format(i, '05d')+'.jpg', rgb_array)
+        cv2.imwrite('out/' + format(i, '05d')+'.jpg', rgb_array)
 
-    frameSize = (x, y)
+    frameSize = (1200, 600)
 
-    out = cv2.VideoWriter('out/output_video.m4v', cv2.VideoWriter_fourcc(*'mp4v'), 10, frameSize)
+    video_out = cv2.VideoWriter('output_video.m4v', cv2.VideoWriter_fourcc(*'mp4v'), 10, frameSize)
 
     for file in glob.glob('out/*.jpg'):
         #print(file)
         img = cv2.imread(file)
-        out.write(img)
+        resized = cv2.resize(img, frameSize)
+        cv2.imwrite(file, resized)
+        resized_jpg = cv2.imread(file)
+        video_out.write(resized_jpg)
 
-    out.release()
+    video_out.release()
 
 
 # Example usage:
